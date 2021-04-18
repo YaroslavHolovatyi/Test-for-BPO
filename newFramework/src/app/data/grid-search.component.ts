f2bb8954b5b9be7b7fc79f678d78509efdbb14d5
YaroslavHolovatyi <yaroslavgolovatyy@gmail.com>
2021-04-18 15:56:04 -0600

    Initial commit

--- newFramework/src/app/data/grid-search.component.ts
+++ newFramework/src/app/data/grid-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { IgxGridComponent} from "igniteui-angular";
-import { ECLASS1, ECLASS2, ECLASS3, ECLASS4, ECLASS5, ECLASS6, ECLASS7, ECLASS8 } from "./data";
+import { ECLASS } from "./data";
 
 @Component({
     selector: "grid-search",
@@ -11,26 +11,12 @@ export class GridSearchComponent implements OnInit {
 
     @ViewChild("grid1", { static: true }) public grid: IgxGridComponent;
     public data: any[];
-    public data1: any[];
-    public data2: any[];
-    public data3: any[];
-    public data4: any[];
-    public data5: any[];
-    public data6: any[];
-    public data7: any[];
     public searchText: string = "";
     public caseSensitive: boolean = false;
     public exactMatch: boolean = false;
 
     public ngOnInit(): void {
-        this.data = ECLASS1;
-        this.data1 = ECLASS2;
-        this.data2 = ECLASS3;
-        this.data3 = ECLASS4;
-        this.data4 = ECLASS5;
-        this.data5 = ECLASS6;
-        this.data6 = ECLASS7;
-        this.data7 = ECLASS8;
+        this.data = ECLASS;
     }
 
     public clearSearch() {
